749dd8c72d7c8df330c624fad2b8920026493137
Meylishh <murmeylishh@gmail.com>
2024-04-06 16:44:56 +0300

    puzzlik for my wife

---
+++ Assets/Script/Puzzles/TileColorPair.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Script.Puzzles
+{
+    [System.Serializable]
+    public class TileColorPair
+    {
+        [field:SerializeField] public ColorTile ColorTile { get; set; }
+        [field:SerializeField] public int CorrectColor { get; set; }
+    }
+}
