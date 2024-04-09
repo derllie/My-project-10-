2e9af9e353c41197c4ef92a849eee78def791f9c
Meylishh <murmeylishh@gmail.com>
2024-04-09 22:27:08 +0300

    two dumb scripts

---
+++ Assets/Script/EndGameTeleport.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameTeleport : MonoBehaviour
+{
+    [SerializeField] private GameObject canvasToShow;
+
+    private void Start()
+    {
+        canvasToShow.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canvasToShow.SetActive(true);
+    }
+}
